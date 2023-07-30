a51e51c6cfa7f74b69a930e3c5197b4018735a71
Jude <38709779+iammrjude@users.noreply.github.com>
2023-07-30 23:05:56 +0100

    Update generate-pdf.jsx

--- pages/generate-pdf.jsx
+++ pages/generate-pdf.jsx
@@ -1,90 +1,79 @@
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import { Table, drawTable } from 'pdf-lib-table';
 
-const generatePDF = async () => {
-  // Create a new PDF document
-  const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage([600, 400]); // Change the size as per your requirement
+const generatePDFWithTable = async () => {
+  const doc = await PDFDocument.create();
+  const page = doc.addPage([600, 400]);
+
+  const table = new Table();
 
   // Define the table properties
   const x = 50; // X-coordinate of the top-left corner of the table
   const y = 350; // Y-coordinate of the top-left corner of the table
   const tableWidth = 500;
   const tableHeight = 200;
-  const numberOfRows = 4;
-  const numberOfColumns = 3;
+  const numberOfRows = 5;
+  const numberOfColumns = 4;
   const cellWidth = tableWidth / numberOfColumns;
   const cellHeight = tableHeight / numberOfRows;
 
-  // Draw the table outline
-  page.drawRectangle({
-    x,
-    y,
-    width: tableWidth,
-    height: tableHeight,
-    borderColor: rgb(0, 0, 0),
-    borderWidth: 2,
-  });
-
-  // Add table content
-  const tableData = [
-    ['Header 1', 'Header 2', 'Header 3'],
-    ['Row 1, Cell 1', 'Row 1, Cell 2', 'Row 1, Cell 3'],
-    ['Row 2, Cell 1', 'Row 2, Cell 2', 'Row 2, Cell 3'],
-    ['Row 3, Cell 1', 'Row 3, Cell 2', 'Row 3, Cell 3'],
+  // Add table content (sample data)
+  const data = [
+    ['Header 1', 'Header 2', 'Header 3', 'Header 4'],
+    ['Row 1, Cell 1', 'Row 1, Cell 2', 'Row 1, Cell 3', 'Row 1, Cell 4'],
+    ['Row 2, Cell 1', 'Row 2, Cell 2', 'Row 2, Cell 3', 'Row 2, Cell 4'],
+    ['Row 3, Cell 1', 'Row 3, Cell 2', 'Row 3, Cell 3', 'Row 3, Cell 4'],
+    ['Row 4, Cell 1', 'Row 4, Cell 2', 'Row 4, Cell 3', 'Row 4, Cell 4'],
   ];
 
-  const fontSize = 12;
-  const textMargin = 5;
-
-  // Use StandardFonts
-  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    for (let colIndex = 0; colIndex < numberOfColumns; colIndex++) {
-      const cellText = tableData[rowIndex][colIndex];
-      const cellX = x + colIndex * cellWidth;
-      const cellY = y - (rowIndex + 1) * cellHeight + cellHeight;
-      page.drawText(cellText, { x: cellX + textMargin, y: cellY - textMargin, size: fontSize, font });
+  // Add rows and cells to the table
+  for (let i = 0; i < numberOfRows; i++) {
+    const row = table.createRow();
+    for (let j = 0; j < numberOfColumns; j++) {
+      const cell = row.createCell(`${data[i][j]}`);
+      cell.setWidth(cellWidth);
+      cell.setPadding(5);
     }
+    table.addRow(row);
   }
 
+  // Set table position and style
+  table.setPosition(x, y);
+  table.setDrawContentCellBorders(true);
+  table.setDrawHeaderRow(true);
+  table.setHeaderRowBackgroundColor(rgb(0.8, 0.8, 0.8));
+
+  // Draw the table on the PDF page
+  drawTable(page, table);
+
   // Save the PDF to a Uint8Array
-  const pdfBytes = await pdfDoc.save();
+  const pdfBytes = await doc.save();
 
   return pdfBytes;
 };
 
-const GeneratePDFPage = ({ pdfBytes }) => {
-  const downloadPDF = () => {
-    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'generated_table.pdf'; // Set the desired file name
-    a.click();
-    URL.revokeObjectURL(url);
-  };
 
+  async function handleGeneratePDFWithTable() {
+  try {
+    const pdfBytes = await generatePDFWithTable();
+    const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+    // Use the library to trigger the download of the PDF
+    saveAs(pdfBlob, 'table.pdf');
+  } catch (error) {
+    console.log('Error generating PDF:', error);
+  }
+}
+
+import { saveAs } from 'file-saver';
+
+const MyPage = () => {
   return (
     <div>
-      <h1>Generated PDF with Table</h1>
-      <button onClick={downloadPDF}>Download PDF</button>
+      <h1>PDF with Table Generation</h1>
+      <button onClick={handleGeneratePDFWithTable}>Generate PDF</button>
     </div>
   );
 };
 
-export async function getServerSideProps() {
-  // Generate the PDF with the table
-  const pdfBytes = await generatePDF();
-
-  // Convert the Uint8Array to a base64 string to pass it as a prop
-  const pdfBase64 = Buffer.from(pdfBytes).toString('base64');
-
-  return {
-    props: {
-      pdfBytes: pdfBase64,
-    },
-  };
-}
-
-export default GeneratePDFPage;
+export default MyPage;
